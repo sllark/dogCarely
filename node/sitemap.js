30375c24fb9e5f4a18ea307c9055133c936809aa
Jack <abdulrehman.6ab@gmail.com>
2021-05-01 10:03:30 +0500

    new post 'elevated alt in dogs'

--- node/sitemap.js
+++ node/sitemap.js
@@ -23,107 +23,112 @@ const links = [
     {
         loc: 'https://www.dogcarely.com',
         priority: '1.00',
-        changefreq: 'weekly',
+        changefreq: 'DAILY',
     },
     {
         loc: 'https://www.dogcarely.com/post/5-best-automatic-dog-feeders-for-large-dogs-under-100.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
     },
     {
         loc: 'https://www.dogcarely.com/post/how-to-take-care-of-a-dog-after-she-gives-birth.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
     },
     {
         loc: 'https://www.dogcarely.com/post/what-should-i-do-if-my-dog-swallowed-a-rawhide.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
     },
     {
         loc: 'https://www.dogcarely.com/post/what-are-the-best-natural-treatments-for-fleas-and-ticks.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
     },
     {
         loc: 'https://www.dogcarely.com/post/5-proven-no-pull-harness-for-your-german-shepherd.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
     },
     {
         loc: 'https://www.dogcarely.com/post/can-dogs-eat-cooked-black-eyed-peas.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
     },
     {
         loc: 'https://www.dogcarely.com/post/can-puppies-drink-milk-that-humans-drink.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
     },
     {
         loc: 'https://www.dogcarely.com/post/how-to-use-coconut-oil-to-remove-dog-tear-stains.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
     },
     {
         loc: 'https://www.dogcarely.com/post/my-dog-ate-a-tampon-and-what-should-I-do-for-treatment-and-prevention.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
     },
     {
         loc: 'https://www.dogcarely.com/post/how-to-make-a-dog-smell-good-without-a-bath.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
     },
     {
         loc: 'https://www.dogcarely.com/post/how-to-help-my-dog-with-milk-fever-aka-eclampsia.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
     },
     {
         loc: 'https://www.dogcarely.com/post/why-are-dogs-ears-so-soft-and-sensitive.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
     },
     {
         loc: 'https://www.dogcarely.com/post/why-dogs-are-aggressive-towards-injured-or-older-dog-and-how-to-correct-this-behavior.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
     },
     {
         loc: 'https://www.dogcarely.com/post/should-i-let-my-dog-see-my-dead-dog.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
     },
     {
         loc: 'https://www.dogcarely.com/post/what-are-dog-weak-spots-to-exploit-if-a-dog-attacks-you.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
     },
     {
         loc: 'https://www.dogcarely.com/post/what-to-do-when-one-dog-died-and-the-other-lives.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
     },
     {
         loc: 'https://www.dogcarely.com/post/why-do-my-dogs-gums-bleed-when-chewing-bone.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
     },
     {
         loc: 'https://www.dogcarely.com/post/can-dogs-die-from-the-leptospirosis-vaccine.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
     },
     {
         loc: 'https://www.dogcarely.com/post/best-mineral-supplement-for-dogs-that-eat-dirt.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
     },
     {
         loc: 'https://www.dogcarely.com/post/how-long-can-a-dog-live-with-high-liver-enzymes.html',
         priority: '0.70',
-        changefreq: 'monthly',
+        changefreq: 'WEEKLY',
+    },
+    {
+        loc: 'https://www.dogcarely.com/post/elevated-alt-in-dogs-why-alt-level-increases-and-what-you-can-do.html',
+        priority: '0.70',
+        changefreq: 'WEEKLY',
     }
 ];
 
